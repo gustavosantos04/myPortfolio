452e23ef3eb740d40d38ed1d78713fee8445255c
Gustavo Santos <santosgustavo0504@gmail.com>
2024-08-06 21:26:54 -0300

    starter commit
    commit para inicio dos estudos

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.getElementById('typing-text');
+    const phrases = [
+        "Desenvolvedor Júnior",
+        "estudante de Ciência da Computação",
+    ];
+    let phraseIndex = 0;
+
+    function typeText(text, callback) {
+        textElement.textContent = '';
+        let index = 0;
+        let interval = setInterval(() => {
+            if (index < text.length) {
+                textElement.textContent += text[index];
+                index++;
+            } else {
+                clearInterval(interval);
+                setTimeout(callback, 1000); // Aguarda 1 segundo antes de chamar o callback
+            }
+        }, 100);
+    }
+
+    function deleteText(callback) {
+        let currentText = textElement.textContent;
+        let length = currentText.length;
+        let interval = setInterval(() => {
+            if (length > 0) {
+                textElement.textContent = currentText.substring(0, length - 1);
+                length--;
+            } else {
+                clearInterval(interval);
+                setTimeout(callback, 500); // Aguarda 0,5 segundo antes de chamar o callback
+            }
+        }, 100);
+    }
+
+    function loopThroughPhrases() {
+        typeText(phrases[phraseIndex], () => {
+            deleteText(() => {
+                phraseIndex = (phraseIndex + 1) % phrases.length;
+                loopThroughPhrases();
+            });
+        });
+    }
+
+    loopThroughPhrases();
+});
