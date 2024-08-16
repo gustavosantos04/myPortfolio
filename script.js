d0ec643352796d04f2fe67d7930bccd03f40b5c6
Gustavo Santos <santosgustavo0504@gmail.com>
2024-08-15 23:28:25 -0300

    a new section of my portfolio

--- script.js
+++ script.js
@@ -72,3 +72,17 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.querySelectorAll('.menu_bar a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scroll({
+            top: targetElement.offsetTop - document.querySelector('.menu_bar').offsetHeight,
+            behavior: 'smooth'
+        });
+    });
+});
