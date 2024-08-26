a1be166d374598b5f4a2e05be0058c47853453f5
Gustavo Santos <santosgustavo0504@gmail.com>
2024-08-26 00:16:15 -0300

    update porfitolio

--- script.js
+++ script.js
@@ -86,3 +86,14 @@ document.querySelectorAll('.menu_bar a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        alert("Please fill out all fields.");
+        event.preventDefault(); // Prevent form submission
+    }
+});
